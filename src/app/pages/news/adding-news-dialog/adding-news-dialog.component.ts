cccdd58218a34ab3ec848b54dd9865907527d1d8
Anton Asipenka <aasipenka@elinext.com>
2022-01-16 03:12:58 +0300

    Added create new news

---
+++ src/app/pages/news/adding-news-dialog/adding-news-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adding-news-dialog',
+  templateUrl: './adding-news-dialog.component.html',
+  styleUrls: ['./adding-news-dialog.component.css']
+})
+export class AddingNewsDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
